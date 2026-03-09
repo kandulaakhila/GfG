a63227b7a04f7017841b87fae23b58d544cbf488
kandulaakhila <akhilakandula735@gmail.com>
2026-03-09 18:32:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.java
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.java
@@ -1,26 +1,23 @@
 class Solution {
     public String largestSwap(String s) {
-        // code here
-        char[] arr=s.toCharArray();
-        int n=arr.length;
-        //store last indexof each digit
-        int[] lastindex=new int[10];
-        for(int i=0;i<n;i++){
-            lastindex[arr[i]-'0']=i;
-        }
-        //traverse and check for largesr digit later
-        for(int i=0;i<n;i++){
-            for(int d=9;d>arr[i]-'0';d--){
-                if(lastindex[d]>i){
-                    //swap
-                    char temp=arr[i];
-                    arr[i]=arr[lastindex[d]];
-                    arr[lastindex[d]]=temp;
-                    return new String(arr);
-                }
-            }
-        }
-        return s;
-        
+       char []arr=s.toCharArray();
+       int n=arr.length;
+       for(int i=0;i<n;i++){
+           int maxDigit=arr[i];
+           int maxIndex=-1;
+       for(int j=i+1;j<n;j++){
+           if(arr[j]>=maxDigit){
+               maxDigit=arr[j];
+               maxIndex=j;
+           }
+       }
+       if(maxIndex!=-1 && arr[maxIndex]>arr[i]){
+           char temp=arr[i];
+           arr[i]=arr[maxIndex];
+           arr[maxIndex]=temp;
+           break;
+       }
+       }
+    return new String(arr);
     }
 }
