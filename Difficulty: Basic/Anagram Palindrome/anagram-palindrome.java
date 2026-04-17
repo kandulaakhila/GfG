5073cc91572337a00628526a711148af68f568c6
kandulaakhila <akhilakandula735@gmail.com>
2026-04-17 09:12:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean canFormPalindrome(String s) {
+        // code here
+        int []freq=new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        int oddcount=0;
+        for(int count:freq){
+            if(count %2!=0){
+                oddcount++;
+            }
+        }
+        return oddcount<=1;
+    }
+}
