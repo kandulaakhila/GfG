cdb2532e84be9569fec8d302c1d146bfd6bc3261
kandulaakhila <akhilakandula735@gmail.com>
2026-05-30 23:34:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Replace with XOR of Adjacent/replace-with-xor-of-adjacent.java
+++ Difficulty: Easy/Replace with XOR of Adjacent/replace-with-xor-of-adjacent.java
@@ -1,19 +1,19 @@
 class Solution {
     public void replaceElements(int[] arr) {
         // code here
-        int n = arr.length;
-        
-        int first = arr[0] ^ arr[1];
-        int last = arr[n - 2] ^ arr[n - 1];
-        int prev = arr[0];
-        
+          int n = arr.length;
+         int prev = arr[0];
+        // Update first element
+        arr[0] = arr[0] ^ arr[1];
+
+        // Update middle elements
         for (int i = 1; i < n - 1; i++) {
             int curr = arr[i];
-            arr[i] = prev ^ arr[i + 1];    
+            arr[i] = prev ^ arr[i + 1];
             prev = curr;
         }
-    
-        arr[0] = first;
-        arr[n - 1] = last;
+
+        // Update last element
+        arr[n - 1] = prev ^ arr[n - 1];
     }
 }
