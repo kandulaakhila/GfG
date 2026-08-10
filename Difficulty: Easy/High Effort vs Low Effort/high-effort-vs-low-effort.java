8db5e78dd768dfb88b88de782d658867735be5b3
kandulaakhila <akhilakandula735@gmail.com>
2026-08-10 10:03:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/High Effort vs Low Effort/high-effort-vs-low-effort.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxTask(int[] h, int[] l) {
+        // code here
+        int n=h.length;
+        int[] dp=new int[n];
+        dp[0]=Math.max(h[0], l[0]);
+        if(n==1)return dp[0];
+        dp[1]=Math.max(dp[0]+l[1],h[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(
+                dp[i-1]+l[i],
+                dp[i-2]+h[i]
+                );
+        }
+        return dp[n-1];
+    }
+}
